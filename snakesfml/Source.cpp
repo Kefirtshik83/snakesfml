962bd39439ddc0b31c875bac3c98271dbf28f4d5
Alexey <kefirtshik83@mail.ru>
2019-04-14 20:46:35 +0300

    Добавлены еще классы

--- snakesfml/Source.cpp
+++ snakesfml/Source.cpp
@@ -1,22 +1,14 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <sstream>
+#include <vector>
 #include <cmath>
-int n = 50, m = 30;
-int scale = 24;
-int d = 20;
-int w = scale * n;
-int hscore = 60;
-int score = 0;
+
 double time1 = 600000;
-int level = 1, maxlevel = 15;
-int h = scale * m;
+
 //1-right  2-up  3-left   4-down
-int dir = 1, len = 5;
 int flag = -1; //start picture
 int i =  5;
-
-
 
 class Picture
 {
@@ -50,83 +42,120 @@ public:
 sf::Clock cl;
 sf::Time t = cl.getElapsedTime();
 
-
-
-class Snake
+class Field
 {
 public:
-	int x;
-	int y;
-} s[600]; // s[0] - head
+	int n = 50, m = 30;
+	int scale = 24;
+	int d = 20;
+	int w = scale * n;
+	int h_field_for_score = 60;
+	int score = 0;
+	int h = scale * m;
 
-class fruit
+} field;
+
+class Fruit
 {
 public:
 	int x;
 	int y;
 } f[6];
 
-void tick()
+struct point
 {
-	if (cl.getElapsedTime().asMicroseconds() - t.asMicroseconds() > time1*pow(0.9, level - 1))
-	{
-		t = cl.getElapsedTime();
+	int x;
+	int y;
+};
 
-		for (int i = len - 1; i > 0; --i)
+class Snake1
+{
+
+public:
+	int level;
+	int dir;
+	int len;
+	std::vector <point> s;
+	sf::Image image;
+	sf::Texture texture;
+
+
+	Snake1(int dir = 1, int len = 2, int level = 1) : dir(dir), len(len), level(level)
+	{ 
+		for (int i = 0; i < len; ++i)
 		{
-			s[i].x = s[i - 1].x;
-			s[i].y = s[i - 1].y;
+			s.push_back({10, 15 + i});
 		}
+		image.loadFromFile("pictures/snake.jpg");
+		texture.loadFromImage(image);
+	}
+	void control() //обработка событий клавиатуры
+	{
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) { dir = 3; }
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) { dir = 1; }
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) { dir = 2; }
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) { dir = 4; }
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::F5)) { flag = 0; }
+	}
+	void tick() //движение змеи, проверка на соударение
+	{
 
-
-
-		if (dir == 1) { s[0].x++; }
-		if (dir == 2) { s[0].y--; }
-		if (dir == 3) { s[0].x--; }
-		if (dir == 4) { s[0].y++; }
-
-		for (int i = 1; i < len; ++i)
+		if (cl.getElapsedTime().asMicroseconds() - t.asMicroseconds() > time1*pow(0.9, level - 1))
 		{
-			if (s[i].x == s[0].x && s[i].y == s[0].y)
+			t = cl.getElapsedTime();
+
+			for (int i = len - 1; i > 0; --i)
+			{
+				s[i].x = s[i - 1].x;
+				s[i].y = s[i - 1].y;
+			}
+
+
+
+			if (dir == 1) { s[0].x++; }
+			if (dir == 2) { s[0].y--; }
+			if (dir == 3) { s[0].x--; }
+			if (dir == 4) { s[0].y++; }
+
+			for (int i = 1; i < len; ++i)
+			{
+				if (s[i].x == s[0].x && s[i].y == s[0].y)
+				{
+					flag = -1;
+					len = 2;
+					s[0].x = 15;
+					s[0].y = 10;
+					field.score = 0;
+				}
+			}
+			if (s[0].x * field.scale + field.d < field.d || s[0].x * field.scale + field.scale + field.d > field.w + field.d || s[0].y * field.scale + field.d < field.d || s[0].y * field.scale + field.scale + field.d> field.h + field.d)
 			{
 				flag = -1;
 				len = 2;
 				s[0].x = 15;
 				s[0].y = 10;
-				score = 0;
+				field.score = 0;
 			}
 		}
-		if (s[0].x * scale + d < d || s[0].x * scale + scale + d > w + d || s[0].y * scale + d < d || s[0].y * scale + scale + d > h + d)
+
+		for (int i = 0; i < 6; ++i)
 		{
-			flag = -1;
-			len = 2;
-			s[0].x = 15;
-			s[0].y = 10;
-			score = 0;
+			if (s[0].x == f[i].x && s[0].y == f[i].y)
+			{
+				s.push_back({ -150,-150 });
+				++field.score;
+				level = 1 + field.score / 1;
+				f[i].x = rand() % field.n; //
+				f[i].y = rand() % field.m;
+				len++;
+				
+
+			}
 		}
 	}
+} snake(1, 2, 1);
 
-	for (int i = 0; i < 6; ++i)
-	{
-		if (s[0].x == f[i].x && s[0].y == f[i].y)
-		{
-			++score;
-			level = 1 + score / 1;
-			f[i].x = rand() % n; //
-			f[i].y = rand() % m;
-			len++;
-			s[len - 1].x = -150;
-			s[len - 1].y = -150;
 
-		}
-	}
-
-}
-
-void key()
-{
-
-}
 
 int main()
 {
@@ -140,21 +169,17 @@ int main()
 
 
 	//headband
-	Picture headband("headband.jpg", 0, 0, 0, 0, w + 2 * d, h + 2 * d + hscore);
+	Picture headband("snake1.jpg", 0, 0, 0, 0, field.w + 2 * field.d, field.h + 2 * field.d + field.h_field_for_score);
 	//start button
-	Picture start_button("start.png", w / 3, h / 3 + score, 50, 50, (w + 2 * d) / 3.1, (h + 2 * d) / 4);
+	Picture start_button("start.png", field.w / 3, field.h / 3 + field.score, 50, 50, (field.w + 2 * field.d) / 3.1, (field.h + 2 * field.d) / 4);
 	start_button.Create_Mask(255, 255, 255, 255);
 	//draw wall
-	sf::RectangleShape wall(sf::Vector2f(w + 2*d, h + hscore + 2*d));
+	sf::RectangleShape wall(sf::Vector2f(field.w + 2 * field.d, field.h + field.h_field_for_score + 2 * field.d));
 	wall.setFillColor(sf::Color(200, 50, 50));
 
 	//draw field
-	Picture field("field.jpg", d, hscore + d, 0, 0, w, h);
-	//texture snake
-	sf::Image snakeim;
-	snakeim.loadFromFile("pictures/snake.jpg");
-	sf::Texture snaketex;
-	snaketex.loadFromImage(snakeim);
+	Picture field1("field.jpg", field.d, field.h_field_for_score + field.d, 0, 0, field.w, field.h);
+
 	//texture apple
 	sf::Image appleim;
 	appleim.loadFromFile("pictures/apple1.png");
@@ -162,19 +187,19 @@ int main()
 	sf::Texture appletex;
 	appletex.loadFromImage(appleim);
 
-	s[0].x = 15;
-	s[0].y = 10;
+	//s[0].x = 15;
+	//s[0].y = 10;
 
 	for (int i = 0; i < 6; ++i)
 	{
-		f[i].x = rand() % n;
-		f[i].y = rand() % m;
+		f[i].x = rand() % field.n;
+		f[i].y = rand() % field.m;
 
 	}
 
 	sf::ContextSettings settings;
 	settings.antialiasingLevel = 8;
-	sf::RenderWindow window(sf::VideoMode(w+2 * d , h+2 * d + hscore), "snake", sf::Style::Default, settings);
+	sf::RenderWindow window(sf::VideoMode(field.w+2 * field.d, field.h+2 * field.d + field.h_field_for_score), "snake", sf::Style::Default, settings);
 
 	while (window.isOpen())
 	{
@@ -191,36 +216,36 @@ int main()
 		{
 			//window.draw(wallsprite);
 			window.draw(wall);
-			window.draw(field.sprite);
+			window.draw(field1.sprite);
 
 
 			std::ostringstream playerScoreString, playerLevelString;    
-			playerScoreString << score;		
+			playerScoreString << field.score;		
 			text.setString("score:" + playerScoreString.str());
-			text.setPosition(0, hscore - d);
+			text.setPosition(0, field.h_field_for_score - field.d);
 			window.draw(text);
-			playerLevelString << level;
+			playerLevelString << snake.level;
 			text.setString("level:" + playerLevelString.str());
-			text.setPosition(w/2, hscore - d);
+			text.setPosition(field.w/2, field.h_field_for_score - field.d);
 			window.draw(text);
 
 			//sf::RectangleShape field(sf::Vector2f(w, h));
 			//draw snake
 			if(flag > 0)
-				tick();
-			for (int i = 0; i < len; ++i)
+				snake.tick();
+			for (int i = 0; i < snake.len; ++i)
 			{
 
-				sf::CircleShape shape(scale/2.f);
-				shape.setPosition(d + s[i].x * scale, d + s[i].y * scale + hscore);
-				shape.setTexture(&snaketex);
+				sf::CircleShape shape(field.scale/2.f); //
+				shape.setPosition(field.d + snake.s[i].x * field.scale, field.d + snake.s[i].y * field.scale + field.h_field_for_score);
+				shape.setTexture(&snake.texture);
 				shape.setTextureRect(sf::IntRect(100, 100, 200, 200));
 				window.draw(shape);
-				if (i < len - 1)
+				if (i < snake.len - 1)
 				{
 					sf::CircleShape shape1(12.f);
-					shape1.setPosition(d + (s[i].x + s[i + 1].x) / 2.0 * scale, d + (s[i].y + s[i + 1].y) / 2.0 * scale + hscore);
-					shape1.setTexture(&snaketex);
+					shape1.setPosition(field.d + (snake.s[i].x + snake.s[i + 1].x) / 2.0 * field.scale, field.d + (snake.s[i].y + snake.s[i + 1].y) / 2.0 * field.scale + field.h_field_for_score);
+					shape1.setTexture(&snake.texture);
 					shape1.setTextureRect(sf::IntRect(100, 100, 200, 200));
 					window.draw(shape1);
 				}
@@ -231,18 +256,14 @@ int main()
 			for (int i = 0; i < 6; ++i)
 			{
 				double k = 1.5;//retewt
-				sf::RectangleShape quad(sf::Vector2f(k * scale, k  * scale));
-				quad.setPosition(d + f[i].x * scale - scale * (k-1)/2, d + f[i].y * scale - scale*(k-1)/2 + hscore);
+				sf::RectangleShape quad(sf::Vector2f(k * field.scale, k  * field.scale));
+				quad.setPosition(field.d + f[i].x * field.scale - field.scale * (k-1)/2, field.d + f[i].y * field.scale - field.scale*(k-1)/2 + field.h_field_for_score);
 				quad.setTexture(&appletex);
 				quad.setTextureRect(sf::IntRect(0, 0, 380, 380));
 				window.draw(quad);
 			}
 
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) { dir = 3; }
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) { dir = 1; }
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) { dir = 2; }
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) { dir = 4; }
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::F5)) { flag = 0; }
+			snake.control();
 		}
 		else
 		{
